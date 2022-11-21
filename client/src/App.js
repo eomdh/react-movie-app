ad74b961d013f68c22a73d968216874ab7b2622a
eomdh <edh9692@gmail.com>
2022-11-21 13:14:23 +0900

    Header 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import { Container } from '@mui/material'
 
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header />
+      <Container maxWidth="lg">
+      </Container>
     </div>
   );
 }
