264d4c18a0d7051dab8639b44371091aae6d0aed
eomdh <edh9692@gmail.com>
2022-11-22 22:43:51 +0900

    Header 수정

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,15 @@ import SearchIcon from '@mui/icons-material/Search';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { styled, alpha } from '@mui/material/styles';
+import icon from '../assets/icon_white.png';
 
 const AppBarStyle = css`
   background-color: black;
+`;
+
+const IconStyle = css`
+  width: 45px;
+  margin-right: 10px;
 `;
 
 const Search = styled('div')(({ theme }) => ({
@@ -47,11 +53,17 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
         width: '10ch',
       },
       [theme.breakpoints.up('sm')]: {
-        width: '20ch',
+        width: '25ch',
+      },
+      [theme.breakpoints.up('lg')]: {
+        width: '40ch',
       }
     },
     [theme.breakpoints.up('xs')]: {
       width: '10ch',
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '20ch',
     },
   },
 }));
@@ -62,12 +74,14 @@ function Header() {
     return (
       <AppBar position="static" css={AppBarStyle} >
         <Container maxWidth="lg">
-          <Toolbar disableGutters>  
+          <Toolbar disableGutters>
+            <img src={icon} alt='icon' css={IconStyle} />
             <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'flex' } }}>
               {pages.map((page) => (
                 <Button
                   key={page}
                   sx={{ my: 2, color: 'white', display: 'block', }}
+                  href={page}
                 >
                   {page}
                 </Button>
