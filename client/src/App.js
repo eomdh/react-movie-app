aae9693955accf9f69c23b451ca97dd0cd13d22a
eomdh <edh9692@gmail.com>
2022-11-25 11:12:37 +0900

    pages 세팅

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import NavBar from './components/NavBar';
 import AppLayout from './components/AppLayout';
 
---
+++ client/src/pages/SearchMovies.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function SearchMovies() {
+  return (
+    <>
+    </>
+  );
+};
+
+export default SearchMovies;
